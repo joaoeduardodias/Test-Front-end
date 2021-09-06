24a3b4675eb083ff8b0d6c77b6d16847674a925d
Joao Dias <joaoeduardodias123@gmail.com>
2021-09-06 14:27:04 -0300

    feat: component Header

---
+++ app/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+
+export function Header(){
+  return (
+   <header className={styles.headerContainer}>
+     <div className={styles.headerContent}>
+        <h1>Healthy Food</h1>
+        <nav>
+          <a>HEALTHY RECIPES</a>
+          <a>BLOG</a>
+          <a>JOIN</a>
+        </nav>
+        
+     </div>
+   </header>
+  )
+}
