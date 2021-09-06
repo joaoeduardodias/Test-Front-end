7e8bc919e2e74e942f3b6128e66e4be2d8552b9d
Joao Dias <joaoeduardodias123@gmail.com>
2021-09-06 15:19:11 -0300

    feat: initial page Home

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { Header } from "../components/Header";
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -6,7 +8,13 @@ export default function Home() {
       <Head>
         <title>Home | Healthy Food</title>
       </Head>
-      <h1>Home</h1>
+      <Header />
+      <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <h1>Ready for<br />Trying a new <br />recipe?</h1>
+          </section>
+          <img src="/Illustration.svg" alt="fruit" />
+      </main>
     </>
   )
 }
